e7e7296b5d5d4b2b7ecf9264b8c861e2a311f4c3
DN <NelubinDO@mail.ru>
2024-04-06 09:23:09 +0300

    Создание массива

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Создание массива на 5 чисел:
+int[] arrey = new int[5];
+arrey[0] = 3;
+arrey[1] = 4;
+arrey[2] = 7;
+arrey[3] = 9;
+arrey[4] = 1;
+Console.WriteLine(arrey[0]);
+Console.WriteLine(arrey[1]);
+Console.WriteLine(arrey[2]);
+Console.WriteLine(arrey[3]);
+Console.WriteLine(arrey[4]);
+
+// Присвоить значения элементам индекса необходимо через фигурные скобки {}
+int[] arrey = [5]{1, 2, 3, 4, 5};
+
+// Создание массива возможно без команды new int[]. Пишем просто:
+int[] arrey = {1, 2, 3, 4, 5};
